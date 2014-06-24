f0a278dd7fe04fc02cff61e8768576fc91540bff
cosmotic@gmail.com <cosmotic@gmail.com@dc451e6c-42ac-3829-5249-a00fcd29e970>
2014-06-24 15:03:08 +0000

    Setup project

---
+++ src/CarpetBallComponent.java
@@ -0,0 +1,5 @@
+import javax.swing.JComponent;
+
+public class CarpetBallComponent extends JComponent {
+
+}
---
+++ src/CarpetBallFrame.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+import java.awt.HeadlessException;
+
+public class CarpetBallFrame extends JFrame {
+	public CarpetBallFrame() throws HeadlessException {
+		super("Carpet Ball");
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+	}
+}
