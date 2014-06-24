9cd89792427e1a99cc494681f0af60d70c8d22cf
cosmotic@gmail.com <cosmotic@gmail.com@dc451e6c-42ac-3829-5249-a00fcd29e970>
2014-06-24 19:45:55 +0000

    Better window size handling

--- trunk/src/CarpetBallComponent.java
+++ trunk/src/CarpetBallComponent.java
@@ -1,10 +1,11 @@
 import javax.swing.JComponent;
+import java.awt.*;
 
 public class CarpetBallComponent extends JComponent {
 
 	private GameState state;
 	public CarpetBallComponent(GameState state) {
-
+        setPreferredSize(new Dimension(300,700));
 		this.state = state;
 	}
 }
