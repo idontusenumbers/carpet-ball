95965a09884feea914e3f812c22f856c7f0316f1
jpitlor <jpitlor@dc451e6c-42ac-3829-5249-a00fcd29e970>
2014-06-26 18:54:26 +0000

    changed isOnBall so you don't have to pass the ball's location into its own method

--- trunk/src/Ball.java
+++ trunk/src/Ball.java
@@ -46,8 +46,9 @@ public class Ball {
         this.velocity = velocity;
     }
 
-	public boolean isOnBall(Point2D ballPos, Point2D otherPos) {
+	public boolean isOnBall(Point2D ballPos) {
 		ballPos.setLocation(ballPos.getX() + Ball.BALL_RADIUS, ballPos.getY() + Ball.BALL_RADIUS);
+		Point2D.Float otherPos = new Point2D.Float((float) getLocation().getX() + Ball.BALL_RADIUS, (float) getLocation().getY() + Ball.BALL_RADIUS);
 		return Point2D.distance(ballPos.getX(), ballPos.getY(), otherPos.getX(), otherPos.getY()) < Ball.BALL_RADIUS;
 	}
 }
